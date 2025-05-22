021b735a3063cdc527e26bda8ffc485f45a3c95e
Ahmed  Eltokhey <ahmdeltoky4@gmail.com>
2025-05-22 04:06:46 +0300

    update header with more details and new app-name

--- components/header.py
+++ components/header.py
@@ -1,5 +1,6 @@
 import streamlit as st
 
 def render_header():
-    st.title(" Background Removal App")
-    st.markdown("Upload an image and remove its background using AI!")
+    st.title("🧼 ClearCut App")
+    st.markdown("Effortlessly remove image backgrounds with AI.")
+    st.markdown("Upload any photo, and our app will give you a clean, transparent background version in seconds!")
