632ed64c61b2080b7d6b91f8690f3842b3e1e3f3
Ahmed  Eltokhey <139669976+ahmdeltoky03@users.noreply.github.com>
2025-05-22 05:55:35 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from components.header import render_header
 from services.image_service import process_and_download_image
+import traceback
 
 st.set_page_config(page_title="Background Removal App", layout="centered")
 
@@ -8,7 +9,15 @@ render_header()
 
 uploaded_file = st.file_uploader(" Upload an image", type=["png", "jpg", "jpeg"])
 
+# if uploaded_file:
+#     process_and_download_image(uploaded_file)
+# else:
+#     st.warning("Please upload an image to get started!")
 if uploaded_file:
-    process_and_download_image(uploaded_file)
+    try:
+        process_and_download_image(uploaded_file)
+    except Exception as e:
+        st.error(f"Error processing image: {e}")
+        st.text(traceback.format_exc())
 else:
     st.warning("Please upload an image to get started!")
