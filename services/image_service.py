96e36db31988ff6332935a22c8569c23cbb87e55
Ahmed  Eltokhey <ahmdeltoky4@gmail.com>
2025-05-22 05:03:39 +0300

    Display original and background-removed images side by side.

--- services/image_service.py
+++ services/image_service.py
@@ -8,33 +8,39 @@ def process_and_download_image(uploaded_file):
     
     # convert the uploaded file to a PIL Image
     image = Image.open(uploaded_file)
-    # Display the original image
-    st.image(image, caption="Original Image", use_container_width=True)
-
+    
     with st.spinner("Removing background..."):
         result_image = remove_bg(uploaded_file)
 
-        st.success("✅ Background removed successfully!")
-        st.image(result_image, caption="Result Image", use_container_width=True)
+    st.success("✅ Background removed successfully!")
 
-        # Prepare download
-        buf = io.BytesIO()
-        result_image.save(buf, format="PNG")
-        buf.seek(0)
+    # Create two columns
+    col1, col2 = st.columns(2)
+
+    with col1:
+        st.image(image, caption="Original Image", use_container_width=True)
+
+    with col2:
+        st.image(result_image, caption="Background Removed", use_container_width=True)
+
+    # Prepare download
+    buf = io.BytesIO()
+    result_image.save(buf, format="PNG")
+    buf.seek(0)
 
 
-        file_basename = uploaded_file.name.rsplit('.', 1)[0]
-        download_filename = os.path.join(file_basename, "no_bg.png")
+    file_basename = uploaded_file.name.rsplit('.', 1)[0]
+    download_filename = os.path.join(file_basename, "no_bg.png")
 
-        st.download_button(
+    st.download_button(
             label=" Download Image",
             data=buf,
             file_name=download_filename,
             mime="image/png"
         )
 
-        # Center using st.markdown and HTML
-        st.markdown(
+    # Center using st.markdown and HTML
+    st.markdown(
             """
             <style>
             div.stDownloadButton > button {
